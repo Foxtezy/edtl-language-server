2cdafce97473fbcf00f5f0dfd71caa2b603e3996
Foxtezy <nmahov2003@gmail.com>
2024-11-18 21:31:02 +0700

    add compare conditions

--- su.nsk.iae.edtl/src/su/nsk/iae/edtl/generator/consistency/ConsistencyGenerator.java
+++ su.nsk.iae.edtl/src/su/nsk/iae/edtl/generator/consistency/ConsistencyGenerator.java
@@ -3,6 +3,7 @@ package su.nsk.iae.edtl.generator.consistency;
 import com.opencsv.CSVWriter;
 
 import org.eclipse.xtext.generator.IFileSystemAccess2;
+import org.logicng.formulas.Formula;
 import org.logicng.formulas.FormulaFactory;
 import org.logicng.util.Pair;
 import su.nsk.iae.edtl.generator.*;
@@ -75,12 +76,84 @@ public class ConsistencyGenerator {
     }
 
     private Answer compare(Req req1, Req req2) {
-        // 13
-        if (f.and(f.implication(req1.reaction(), req2.fin()),
-                f.not(f.and(req1.reaction(), req2.invariant())),
-                f.not(f.implication(f.or(req2.fin(), req1.reaction()), f.or(req2.reaction(), req2.release())))).isTautology()) {
+        Formula trg = f.or(f.and(req1.trigger(), f.not(req1.release())), req1.invariant(), f.and(req2.trigger(), f.not(req2.release())));
+
+        // 1
+        if (f.and(
+                f.not(f.and(req1.invariant(), req2.invariant())),
+                f.implication(trg, req1.release()),
+                f.implication(trg, f.and(req1.fin(), req1.reaction())),
+                f.not(f.implication(trg, f.or(req2.release(), f.and(req2.fin(), req2.reaction()))))
+        ).isTautology()) {
             return Answer.INCONSISTENT;
         }
+
+        // 2
+        if (f.and(
+                f.not(f.and(req1.release(), req2.invariant())),
+                f.implication(trg, req1.release()),
+                f.not(f.implication(trg, f.or(req2.release(), f.and(req2.fin(), req2.reaction()))))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+
+        // 3
+        if (f.and(
+                f.not(f.and(f.and(req1.fin(), req1.reaction()), req2.invariant())),
+                f.implication(trg, f.and(req1.fin(), req1.reaction())),
+                f.not(f.implication(trg, f.or(req2.release(), f.and(req2.fin(), req2.reaction()))))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+        
+        // 4
+        if (f.and(
+                f.implication(req1.release(), req2.fin()),
+                f.not(f.and(req1.release(), req2.invariant())),
+                f.not(f.implication(f.or(req1.release(), req2.fin()), f.or(req2.release(), req2.reaction())))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+
+        // 5
+        if (f.and(
+                f.implication(req1.reaction(), req2.fin()),
+                f.not(f.and(req1.reaction(), req2.invariant())),
+                f.not(f.implication(f.or(req1.reaction(), req2.fin()), f.or(req2.release(), req2.reaction())))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+
+        // 6
+        if (f.and(
+                f.implication(req1.fin(), req2.fin()),
+                f.not(f.and(req1.fin(), req2.invariant())),
+                f.not(f.implication(f.or(req1.fin(), req2.fin()), f.or(req2.release(), req2.reaction())))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+
+        // 7
+        if (f.and(
+                f.implication(req1.fin(), req2.fin()),
+                f.not(f.and(req1.invariant(), f.not(req2.delay()))),
+                f.not(f.and(f.or(req1.invariant(), req1.fin()), req2.release())),
+                f.not(f.and(f.or(req1.invariant(), req1.fin()), req2.reaction()))
+        ).isTautology()) {
+            return Answer.INCONSISTENT;
+        }
+
+        // CONSISTENT
+        if (f.and(
+                f.implication(req1.invariant(), req2.invariant()),
+                f.implication(req1.fin(), req2.fin()),
+                f.implication(req1.delay(), req2.delay()),
+                f.implication(req1.reaction(), req2.reaction()),
+                f.implication(req1.release(), req2.release())
+        ).isTautology()) {
+            return Answer.CONSISTENT;
+        }
+
         return Answer.UNKNOWN;
     }
 
